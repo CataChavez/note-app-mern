6c93b18b6e2250129469d04a0032249fb99a2867
Catty <kata.chavez@gmail.com>
2021-08-31 17:30:46 -0400

    Navbar and routes add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import NavBar from './containers/navBar/NavBar.js';
+import NoteList from './components/NoteList';
+import CreateNote from './components/CreateNote';
+import CreateUser from './components/CreateUser';
+
 
 function App() {
   return (
-   <div>
-     Hello world!
+   <div className="">
+     <Router>
+        <NavBar />
+        <Route path="/" exact component={NoteList}></Route>
+        <Route path="/edit/:id" component={CreateNote}></Route>
+        <Route path="/create" component={CreateNote}></Route>
+        <Route path="/user" component={CreateUser}></Route>
+
+
+     </Router>
+
    </div>
   );
 }
